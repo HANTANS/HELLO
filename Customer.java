8aa00d3f928aa838c93fa18b812651cc41cd7465
HANTANS <2282832385@qq.com>
2022-05-27 16:23:48 +0800

    master

--- Customer.java
+++ Customer.java
@@ -9,7 +9,7 @@ public class Customer{
     public void Cust(ArrayList<Customer> cust){
         System.out.println("数量 商品");
         for(int i=0;i<cust.size()-1;i++){
-            System.out.println(cust.get(i).num+" "+cust.get(i).goodname);
+            System.out.println(cust.get(i).num+"    "+cust.get(i).goodname);
         }
         System.out.println("总计:"+cust.get(cust.size()-1).price+"元");
     }
